b0c58674c6f67cb927a3ae9346b795fcb38567eb
Jpergon251 <Jpergon251@g.educaand.es>
2023-09-29 12:50:09 +0200

    hasta el ejercicio 16

--- DWEServidor/actividades java/actividad03.java
+++ DWEServidor/actividades java/actividad03.java
@@ -1,15 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class actividad03 {
     /*Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for. */
     public static void multiplosFor() {
-        System.out.println("Números múltiplos de 5 de 0 a 100:");
+        StringBuilder resultado = new StringBuilder("Números múltiplos de 5 de 0 a 100:\n");
 
         for (int numero = 0; numero <= 100; numero++) {
             if (numero % 5 == 0) {
-                System.out.println(numero);
+                resultado.append(numero).append(", ");
             }
         }
+        System.out.println(resultado);
     }
 
     /*Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle While. */
@@ -154,9 +157,9 @@ public class actividad03 {
 
         int numero1 = 0;
         int numero2 = 1;
-        String resultado = numero1 + ", ";
-        for (int i = 0; i < numero; i++) {
-            resultado += numero2 + ", ";
+        StringBuilder resultado = new StringBuilder(numero1 + ", ");
+        for (int i = 0; i < numero-1; i++) {
+            resultado.append(numero2).append(", ");
             int numero3 = numero1 + numero2;
             numero1 = numero2;
             numero2 = numero3;
@@ -165,25 +168,103 @@ public class actividad03 {
     }
 
     /*Escribe un programa que lea una lista de diez números y determine cuántos son positivos, y cuántos son negativos. */
-    public static void listaNumeros(int[] numeros){
-        String positivos = "Los numeros positivos de la lista son: "
-        String negativos = "Los numeros negativos de la lista son: "
-        for (int numero = 0; numero < numeros.length; numero++){
-            if (numero >= 0){
-                positivos += numero + ", ";
-            }else{
-                negativos += numero + ", ";
+    public static void listaNumeros(List<Integer> numeros){
+        StringBuilder positivos = new StringBuilder("Los numeros positivos de la lista son: ");
+        StringBuilder negativos = new StringBuilder("Los numeros negativos de la lista son: ");
+        for (int i : numeros) {
+            if (i >= 0) {
+                positivos.append(i).append(", ");
+            } else {
+                negativos.append(i).append(", ");
             }
         }
         System.out.println(positivos + "\n" + negativos);
     }
 
-    public static void main(String[] args) {
+    /*Escribe un programa que pida una base y un exponente (entero positivo) y que calcule la potencia.*/
+    public static void potencia(int base, int exponente){
+        int conteo = 0;
+        int resultado = 1;
+        do{
+            resultado *= base;
+        }while (conteo < exponente);
+        System.out.println(base + " exponenete "+ exponente + " = " + resultado);
+    }
+
+    /*Escribe un programa que dados dos números, uno real (base) y un entero positivo (exponente),
+    saque por pantalla todas las potencias con base el numero dado y exponentes entre uno y el exponente introducido.
+    No se deben utilizar funciones de exponenciación. Por ejemplo, si introducimos
+    el 2 y el 5, se deberán mostrar 2 1 , 2 2 , 2 3 , 2 4 y 2 5 .*/
+    public static void exponentes(int base, int exponente){
+        StringBuilder resultado = new StringBuilder("La cadena de exponentes es: ");
+        for (int i = 1; i < exponente; i++) {
+            resultado.append(base).append(i).append(", ");
+        }
+        System.out.println(resultado);
+    }
+
+    /*Escribe un programa que diga si un número introducido por teclado es o no primo. Un número
+    primo es aquel que sólo es divisible entre él mismo y la unidad.*/
+    public static void confirmarPrimo(int numero){
+        boolean esPrimo = true;
+
+        for (int i = 2; i < numero; i++) {
+            if (numero % i == 0) {
+                esPrimo = false;
+                System.out.println("El número no es primo");
+                break;
+            }
+        }
+        if (esPrimo) {
+            System.out.println("El número es primo");
+        }
+    }
+
+   /* Realiza un programa que sume los 100 números siguientes a un número entero y positivo introducido por teclado.
+    Se debe comprobar que el dato introducido es correcto (que es un número positivo).*/
+
+   /*Escribe un programa que obtenga los números enteros comprendidos entre dos números intro-
+    ducidos por teclado y validados como distintos, el programa debe empezar por el menor de los
+    enteros introducidos e ir incrementando de 7 en 7.*/
+
+   /*Realiza un programa que pinte una pirámide por pantalla. La altura se debe pedir por teclado. El
+    carácter con el que se pinta la pirámide también se debe pedir por teclado.*/
+
+    /*Igual que el ejercicio anterior pero esta vez se debe pintar una pirámide hueca.*/
+
+    /*Realiza un programa que vaya pidiendo números hasta que se introduzca un numero negativo y
+    nos diga cuantos números se han introducido, la media de los impares y el mayor de los pares. El
+    número negativo sólo se utiliza para indicar el final de la introducción de datos pero no se incluye
+    en el cómputo.*/
+
+    /*Muestra por pantalla todos los números primos entre 2 y 100, ambos incluidos*/
+    public static void primos(){
+        System.out.println("Números primos entre 2 y 100:");
+        boolean esPrimo = true;
+        StringBuilder resultado = new StringBuilder("Los números primos son:  ");
+
+        for (int numero = 2; numero <= 100;numero++) {
+            esPrimo = true;
+            for (int i = 2; i < numero; i++) {
+                if (numero % i == 0) {
+                    esPrimo = false;
+                    break;
+                }
+            }
+            if (esPrimo) {
+                resultado.append(numero).append(", ");
+            }
+        }
+        System.out.println(resultado);
+    }
+
+    public static <Int> void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             int actividad = 1;
             while (actividad > 0){
                 System.out.print("Seleccione la actividad a ejecutar: ");
                 actividad = scanner.nextInt();
+                int numero;
                 switch(actividad){
                     case 0:
                         System.out.println("Dejando las actividades del pdf 03.");
@@ -210,12 +291,12 @@ public class actividad03 {
                         cajaFuerte();
                         break;
                     case 8:
-                        System.out.print("Introduzca un numero: ");
-                        int numero = scanner.nextInt();
+                        System.out.print("Introduzca un número entero: ");
+                        numero = scanner.nextInt();
                         tablaMultiplicar(numero);
                         break;
                     case 9:
-                        System.out.print("Introduzca un numero: ");
+                        System.out.print("Introduzca un número entero: ");
                         numero = scanner.nextInt();
                         digitosNumero(numero);
                         break;
@@ -233,16 +314,34 @@ public class actividad03 {
                         serieFibonacci(numero);
                         break;
                     case 13:
-                        
+                        int cantidadNumeros = 0;
+                        List<Integer> numeros = new ArrayList<>();
+                        while (cantidadNumeros < 10) {
+                            System.out.print("Introduzca un numero para añadir a la lista: ");
+                            numero = scanner.nextInt();
+                            numeros.add(numero);
+                            cantidadNumeros++;
+                        }
+                        listaNumeros(numeros);
                         break;
                     case 14:
-                        
+                        System.out.print("Introduce un número entero: ");
+                        numero = scanner.nextInt();
+                        System.out.print("Introduce su exponente: ");
+                        int exponente = scanner.nextInt();
+                        potencia(numero, exponente);
                         break;
                     case 15:
-                        
+                        System.out.print("Introduce un número entero: ");
+                        numero = scanner.nextInt();
+                        System.out.print("Introduce su exponente: ");
+                        exponente = scanner.nextInt();
+                        exponentes(numero, exponente);
                         break;
                     case 16:
-                        
+                        System.out.print("Introduce un número entero: ");
+                        numero = scanner.nextInt();
+                        confirmarPrimo(numero);
                         break;
                     case 17:
                         
@@ -260,7 +359,7 @@ public class actividad03 {
                         
                         break;
                     case 22:
-
+                        primos();
                         break;
                 }
             }
