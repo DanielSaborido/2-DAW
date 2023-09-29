7af70d11403fafafb0e28966b5acfb3995b3b514
DanielSaborido <114474965+DanielSaborido@users.noreply.github.com>
2023-09-29 10:18:37 +0200

    reajuste de codigo

--- DWECliente/actividadesJavaScript/java/ejercicio4.js
+++ DWECliente/actividadesJavaScript/java/ejercicio4.js
@@ -1,7 +1,5 @@
 function mostrarMeses() {
     var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
-    for (var i = 0; i < meses.length; i++) {
-        alert(meses[i]);
-    }
+    meses.forEach((mes) => alert(mes))
 }
