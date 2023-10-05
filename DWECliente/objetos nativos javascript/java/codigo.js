ea14e2d07aee9be39aecabd24164bad4ad23dc66
DanielSaborido <dsaborio@iesrafaelalberti.es>
2023-10-05 21:57:32 +0200

    actividades javascript A02 2 primeras actividades

--- DWECliente/objetos nativos javascript/java/codigo.js
+++ DWECliente/objetos nativos javascript/java/codigo.js
@@ -1,3 +1,9 @@
-//este comando lo que hace es que muestre lo que esta dentro del parentesis por consola
-console.log("Hello World!");
-console.log("Soy el primer script");
+const radio = parseFloat(prompt("Dame el radio de la circunferencia"));
+const diametro = 2*radio;
+const perimetro = 2 * Math.PI * radio;
+const areaC = Math.PI * radio ** 2;
+const areaE = 4 * Math.PI * radio ** 2;
+const volumen = (4 / 3) * Math.PI * radio ** 3;
+
+document.getElementById("Result").innerHTML = `Radio: ${radio.toFixed(2)}<br>Diametro: ${diametro.toFixed(2)}<br>
+Perimetro: ${perimetro.toFixed(2)}<br>Area Circulo: ${areaC.toFixed(2)}<br>Area Esfera: ${areaE.toFixed(2)}<br>Volumen Esfera: ${volumen.toFixed(2)}`;
--- DWECliente/objetos nativos javascript/java/ejercicio2.js
+++ DWECliente/objetos nativos javascript/java/ejercicio2.js
@@ -1,3 +1,29 @@
-mensaje = "JavaScript \nHola Mundo! \nQue facil incluir \'comillas simples\' \ny \"comillas dobles\"";
+const usuario = prompt("Inserte su nombre y apellidos")
 
-alert(mensaje);
+function dividion(usuario) {
+    let texto= ``;
+    usuario.split(' ').forEach(dato => {
+        texto+=`<li>${dato}</li>`;
+    })
+    return texto;
+}
+function sugerencia(usuario) {
+    let texto= ``;
+    const desglose = usuario.split(' ');
+    desglose.forEach((dato, index) => {
+        if(index===0 || index===desglose.length-1){
+            texto+=dato.slice(0,1);
+        } else {texto+=dato}
+    })
+    return texto.toLowerCase();
+}
+function tresLetras(usuario) {
+    let texto= ``;
+    usuario.split(' ').forEach(dato => {
+        texto+=dato.slice(0,3);
+    })
+    return texto.charAt(0).toUpperCase() + texto.slice(1).toLowerCase();
+}
+
+document.getElementById("Result").innerHTML = `Longitud: ${(usuario.split(' ').join('')).length}<br>Minuscula: ${usuario.toLowerCase()}<br>
+Mayuscula: ${usuario.toUpperCase()}<br>${dividion(usuario)}${sugerencia(usuario)}<br>${tresLetras(usuario)}`;
