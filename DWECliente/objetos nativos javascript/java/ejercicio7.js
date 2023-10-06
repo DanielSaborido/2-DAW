9b48b08a13d8d1f2d1794a3ba71173c5b87ff8ac
DanielSaborido <dsaborio@iesrafaelalberti.es>
2023-10-06 13:45:41 +0200

    ajustes en el 7 para hacerlo mas divertido

--- DWECliente/objetos nativos javascript/java/ejercicio7.js
+++ DWECliente/objetos nativos javascript/java/ejercicio7.js
@@ -1,30 +1,40 @@
+let ventanas = [];
 let ventana;
 let width = 400;
 let height = 400;
 let shrinking = true;
+let intervalIniciado = false;
 
 function abrirVentana() {
-    if (ventana && !ventana.closed) {
-        ventana.focus();
-        return;
+    ventanas.forEach((ventana) => {
+        if (ventana && !ventana.closed) {
+            ventana.focus();
+        }
+    });
+    if (!intervalIniciado) {
+        setInterval(() => {
+            if (shrinking) {
+                width -= 40;
+                height -= 40;
+            } else {
+                width += 40;
+                height += 40;
+            }
+            ventanas.forEach((ventana) => {
+                ventana.resizeTo(width, height);
+            });
+            if (width <= 160 && height <= 160) {
+                shrinking = false;
+            }
+            if (width >= 400 && height >= 400) {
+                shrinking = true;
+            }
+        }, 1000);
+
+        intervalIniciado = true;
     }
     const left = Math.floor(Math.random() * (window.innerWidth - width));
     const top = Math.floor(Math.random() * (window.innerHeight - height));
     ventana = window.open('', '', `width=${width}, height=${height}, left=${left}, top=${top}`);
-    setInterval(() => {
-        if (shrinking) {
-            width -= 40;
-            height -= 40;
-        } else {
-            width += 40;
-            height += 40;
-        }
-        ventana.resizeTo(width, height);
-        if (width <= 160 && height <= 160) {
-            shrinking = false;
-        }
-        if (width >= 400 && height >= 400) {
-            shrinking = true;
-        }
-    }, 750);
+    ventanas.push(ventana);
 }
