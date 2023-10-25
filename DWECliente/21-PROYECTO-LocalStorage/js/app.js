1fa7eb3d92e13cd92395066a10b66d2c0d836b55
DanielSaborido <dsaborio@iesrafaelalberti.es>
2023-10-25 21:51:51 +0200

    base de las especificaciones terminado

--- DWECliente/21-PROYECTO-LocalStorage/js/app.js
+++ DWECliente/21-PROYECTO-LocalStorage/js/app.js
@@ -1,50 +1,81 @@
-const mensajeTextarea = document.querySelector("textarea")
-const envio = document.querySelector(".button")
-const listaMensajes = document.querySelector("#lista-mensajes")
+const mensajeTextarea = document.querySelector("textarea");
+const enviar = document.querySelector("input[value='Agregar']");
+const vaciar = document.querySelector("input[value='Vaciar']");
+const listaMensajes = document.querySelector("#lista-mensajes");
 
-let coleccionMensajes = []
+let coleccionMensajes = [];
 
 function listeners() {
     document.addEventListener("DOMContentLoaded", () => {
-        const mensajes = JSON.parse(localStorage.getItem("mensajes"))
-        if (mensajes.length){
-            articulosCarrito = mensajes
-            mostrarMensajes(mensajes)
+        const mensajes = JSON.parse(localStorage.getItem("mensajes"));
+        if (mensajes && mensajes.length) {
+            coleccionMensajes = mensajes;
+            mostrarMensajes(mensajes);
         }
-    })
-    envio.addEventListener('submit', anadirMensaje)
-}
-listeners()
+    });
 
-function anadirMensaje(e){
+    enviar.addEventListener('click', anadirMensaje);
+    vaciar.addEventListener('click', eliminarDatos);
+    listaMensajes.addEventListener('click', eliminarMensaje);
+}
+
+listeners();
+
+function eliminarDatos(e) {
+    e.preventDefault();
+    coleccionMensajes = [];
+    almacenLocal(coleccionMensajes);
+}
+
+function anadirMensaje(e) {
+    e.preventDefault();
+    const mensaje = mensajeTextarea.value;
+    console.log("Agregando...", mensaje);
+    coleccionMensajes.push(mensaje);
+    almacenLocal(coleccionMensajes);
+    mensajeTextarea.value = "";
+}
+
+function almacenLocal(lista) {
+    localStorage.setItem("mensajes", JSON.stringify(lista));
+    mostrarMensajes(lista);
+}
+
+function mostrarMensajes(mensajes) {
+    limpiarHTML();
+    let index = 0;
+    mensajes.forEach((mensaje) => {
+        const texto = document.createElement("p");
+        texto.textContent = `Mensaje ${index + 1}: ${mensaje}`;
+        const borrado = document.createElement("a");
+        borrado.classList.add("borrar-mensaje");
+        borrado.setAttribute("data-id", index);
+        borrado.textContent = "X";
+        listaMensajes.appendChild(texto);
+        listaMensajes.appendChild(borrado);
+        index++;
+    });
+}
+
+function eliminarMensaje(e) {
     e.preventDefault()
-    if (e.target.classList.contains("#button")){
-        console.log("Agregando...")
-        const mensaje = mensajeTextarea.values
-        console.log(mensaje)
-        coleccionMensajes = [...coleccionMensajes, mensaje]
+    console.log(e.target)
+    console.log(e.target.classList.contains("borrar-mensaje"))
+    if (e.target.classList.contains("borrar-mensaje")){
+        console.log("Borando...")
+        const mensajeID = e.target.getAttribute("data-id")
+        console.log(mensajeID)
+
+        coleccionMensajes = coleccionMensajes.filter((mensaje, index)=> 
+            index !== parseInt(mensajeID )
+        )
+        console.log(coleccionMensajes)
         almacenLocal(coleccionMensajes)
     }
 }
 
-function almacenLocal(lista){
-    localStorage.setItem("mensajes", JSON.stringify(lista))
-    mostrarMensajes(JSON.parse(localStorage.getItem("mensajes")))
-}
-
-function mostrarMensajes(mensajes){
-    limpiarHTML()
-    let index = 1
-    mensajes.forEach((mensaje)=> {
-        const texto = document.createElement("p")
-        texto.innerHTML = `Mensaje ${index}: ${mensaje}`
-        listaMensajes.appendChild(texto)
-        index++;
-    })
-}
-
 function limpiarHTML() {
-    while (listaMensajes.firstChild){
-        listaMensajes.firstChild.remove()
+    while (listaMensajes.firstChild) {
+        listaMensajes.firstChild.remove();
     }
 }
