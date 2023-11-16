14e2d74578b32aa0d535ab32dee8ac6d4c3d81b2
DanielSaborido <dsaborio@iesrafaelalberti.es>
2023-11-16 10:23:43 +0100

    eliminación de codifgo sobrante

--- DWECliente/30-PROYECTO-Clima/js/app1.js
+++ DWECliente/30-PROYECTO-Clima/js/app1.js
@@ -8,6 +8,7 @@ const resultado = document.querySelector('#resultado');
 window.addEventListener('load', () => {
     formulario.addEventListener('submit', buscarClima)
 })
+
 function buscarClima(e) {
     e.preventDefault()
     console.log('Clima')
@@ -19,14 +20,11 @@ function buscarClima(e) {
         return
     }
     consultarAPI(ciudad, pais)
-
 };
 
 function mostrarError(mensaje) {
     const alerta = document.createElement('div');
-    
     alerta.classList.add('bg-red-100', "border-red-400", "text-red-700", "px-4", "py-3", "rounded", "relative", "max-w-md", "mx-auto", "mt-6", "text-center" );
-    
     alerta.innerHTML=`
     <strong class='font-bold'>Error!! </strong>
     <span class='block'> ${mensaje}</span>
@@ -52,6 +50,7 @@ function consultarAPI(ciudad, pais){
         mostrarClima(data)
     })
 }
+
 function mostrarClima(clima){
     const {
         main: {temp, temp_max, temp_min },
@@ -94,25 +93,4 @@ function limpiarHTML(){
     while(resultado.firstChild){
         resultado.firstChild.remove();
     }
-}
-
-function mostrarSpinner(){
-    limpiarHTML()
-    const divSpinner = document.createElement('div')
-    divSpinner.classList.add('sk-fading-circle')
-
-    divSpinner.innerHTML = `<div class="sk-circle">
-    <div class="sk-circle1 sk-child"></div>
-    <div class="sk-circle2 sk-child"></div>
-    <div class="sk-circle3 sk-child"></div>
-    <div class="sk-circle4 sk-child"></div>
-    <div class="sk-circle5 sk-child"></div>
-    <div class="sk-circle6 sk-child"></div>
-    <div class="sk-circle7 sk-child"></div>
-    <div class="sk-circle8 sk-child"></div>
-    <div class="sk-circle9 sk-child"></div>
-    <div class="sk-circle10 sk-child"></div>
-    <div class="sk-circle11 sk-child"></div>
-    <div class="sk-circle12 sk-child"></div>
-    </div>`
 }
