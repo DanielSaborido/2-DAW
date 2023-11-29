a36c56e4e43ca8c0c0fac321974ba7aea8f3110b
DanielSaborido <dsaborio@iesrafaelalberti.es>
2023-11-29 08:57:19 +0100

    ajustes esteticos

--- DWECliente/experimento api/js/01-app.js
+++ DWECliente/experimento api/js/01-app.js
@@ -2,20 +2,20 @@ const senda = document.querySelector('#senda')
 const habilidades = document.querySelector('#habilidad')
 const razas = document.querySelector('#raza')
 const contenido = document.querySelector("#contenido")
+const filtro = document.querySelector("#filtros")
+const modal = new bootstrap.Modal('#modalROL', {})
 
 function listeners() {
     document.addEventListener("DOMContentLoaded", () => {
         crearDB()
         obtenerDB()
+        filtro.reset()
     })
 
-    senda.addEventListener('input', () => {
+    filtro.addEventListener('input', () => {
         obtenerDB()
     })
-    habilidades.addEventListener('input', () => {
-        obtenerDB()
-    })
-    razas.addEventListener('input', () => {
+    filtro.addEventListener('reset', () => {
         obtenerDB()
     })
 
@@ -23,12 +23,12 @@ function listeners() {
         e.preventDefault()
         const rolOBJ = {
             imagen: "img/" + document.querySelector('#imagen').files[0].name,
-            nombreCurso: document.querySelector('#nombreCurso').value,
-            autor: document.querySelector('#autor').value,
-            puntuacion: "img/estrellas.png",
-            precio: document.querySelector('#precio').value
+            nombreRol: document.querySelector('#nombreCurso').value,
+            senda: document.querySelector('#autor').value,
+            habilidades: "img/estrellas.png",
+            razas: document.querySelector('#precio').value
         }
-        insertarCurso(rolOBJ)
+        insertarRol(rolOBJ)
         obtenerDB()
         formulario.reset()
     })
@@ -69,7 +69,7 @@ function mostrarHTML(roles){
         titulo.textContent = 'Roles de Skyrim'
         contenido.appendChild(titulo)
         roles.forEach(rol => {
-            const { id, dataRol : {imagen, nombreRol, senda, habilidades, razas} } = rol
+            const { id, dataRol : {imagen, nombreRol, descripcion} } = rol
                 const contenedorRol = document.createElement("div")
                 contenedorRol.classList.add('card')
 
@@ -82,42 +82,23 @@ function mostrarHTML(roles){
                 infoContainer.classList.add('info-card')
 
                 const nombre = document.createElement("h4")
+                nombre.classList.add('u-full-width')
                 nombre.textContent = nombreRol
 
-                const tituloSenda = document.createElement("h4")
-                tituloSenda.textContent = "Senda"
-                const listaSendas = document.createElement("ul")
-                senda.forEach(senda =>{
-                    const elemento = document.createElement("li")
-                    elemento.textContent = senda
-                    listaSendas.appendChild(elemento)
-                })
+                const descripcionRol = document.createElement("p")
+                descripcionRol.textContent = descripcion
 
-                const tituloHabilidad = document.createElement("h4")
-                tituloHabilidad.textContent = "Habilidades"
-                const listaHabilidades = document.createElement("ul")
-                habilidades.forEach(habilidad =>{
-                    const elemento = document.createElement("li")
-                    elemento.textContent = habilidad
-                    listaHabilidades.appendChild(elemento)
-                })
-
-                const tituloRazas = document.createElement("h4")
-                tituloRazas.textContent = "Razas"
-                const listaRazas = document.createElement("ul")
-                razas.forEach(raza =>{
-                    const elemento = document.createElement("li")
-                    elemento.textContent = raza
-                    listaRazas.appendChild(elemento)
-                })
+                const rolButton = document.createElement('button')
+                rolButton.classList.add('btn', 'btn-danger', 'w-100')
+                rolButton.textContent = 'Mas informacion'
+                rolButton.onclick = function(){
+                    console.log(rol)
+                    mostrarRolModal(rol)
+                }
                 
                 infoContainer.appendChild(nombre)
-                infoContainer.appendChild(tituloSenda)
-                infoContainer.appendChild(listaSendas)
-                infoContainer.appendChild(tituloHabilidad)
-                infoContainer.appendChild(listaHabilidades)
-                infoContainer.appendChild(tituloRazas)
-                infoContainer.appendChild(listaRazas)
+                infoContainer.appendChild(descripcionRol)
+                infoContainer.appendChild(rolButton)
                 contenedorRol.appendChild(imagenRol)
                 contenedorRol.appendChild(infoContainer)
                 contenido.appendChild(contenedorRol)
@@ -129,6 +110,119 @@ function limpiarHTML(selector) {
     while (selector.firstChild) {
         selector.firstChild.remove()
     }
+}
+
+function mostrarRolModal(rol){
+    const { id, dataRol : {imagen, nombreRol, senda, habilidades, razas} } = rol
+    const modalContent = document.querySelector('#modalROL')
+    const modalTitle = document.querySelector('#modalROL .modal-title')
+    modalTitle.textContent = nombreRol
+
+    const modalBody = document.querySelector('#modalROL .modal-body')
+    limpiarHTML(modalBody)
+    const imagenRol = document.createElement("img")
+    imagenRol.classList.add('imagen-curso', 'u-full-width')
+    imagenRol.alt = `Imagen del Rol ${nombreRol}`
+    imagenRol.src = imagen
+
+    const tituloSenda = document.createElement("h6")
+    tituloSenda.classList.add('font-bold')
+    tituloSenda.textContent = "Senda"
+    const listaSendas = document.createElement("ul")
+    senda.forEach(senda =>{
+        const elemento = document.createElement("li")
+        elemento.textContent = senda
+        listaSendas.appendChild(elemento)
+    })
+
+    const tituloHabilidad = document.createElement("h6")
+    tituloHabilidad.classList.add('font-bold')
+    tituloHabilidad.textContent = "Habilidades"
+    const listaHabilidades = document.createElement("ul")
+    habilidades.forEach(habilidad =>{
+        const elemento = document.createElement("li")
+        elemento.textContent = habilidad
+        listaHabilidades.appendChild(elemento)
+    })
+
+    const tituloRazas = document.createElement("h6")
+    tituloRazas.classList.add('font-bold')
+    tituloRazas.textContent = "Razas"
+    const listaRazas = document.createElement("ul")
+    razas.forEach(raza =>{
+        const elemento = document.createElement("li")
+        elemento.textContent = raza
+        listaRazas.appendChild(elemento)
+    })
+    
+    modalBody.appendChild(imagenRol)
+    modalBody.appendChild(tituloSenda)
+    modalBody.appendChild(listaSendas)
+    modalBody.appendChild(tituloHabilidad)
+    modalBody.appendChild(listaHabilidades)
+    modalBody.appendChild(tituloRazas)
+    modalBody.appendChild(listaRazas)
+
+    const modalFooter = document.querySelector('#modalROL .modal-footer')
+    limpiarHTML(modalFooter)
+
+    const btnFavoritos = document.createElement('button')
+    btnFavoritos.classList.add('btn', 'col')
+    const favoritos = JSON.parse(localStorage.getItem('favoritos')) || []
+    if (favoritos.includes(id)) {
+        btnFavoritos.classList.add('btn-warning')
+        btnFavoritos.textContent = 'Borrar Favorito'
+    } else {
+        btnFavoritos.classList.add('btn-danger')
+        btnFavoritos.textContent = 'Guardar Favoritos'
+    }
+
+    const btnCerrar = document.createElement('button')
+    btnCerrar.classList.add('btn', 'btn-secondary', 'col')
+    btnCerrar.textContent = 'Cerrar'
+
+    modalFooter.appendChild(btnFavoritos)
+    modalFooter.appendChild(btnCerrar)
+
+    modalContent.addEventListener('click', function(event) {
+        const cursor = event.target
+        if(cursor === btnFavoritos){
+            if (favoritos.includes(id)) {
+                removerFavorito(id)
+                mostrarToast('Receta eliminada de favoritos')
+                btnFavoritos.classList.remove('btn-warning')
+                btnFavoritos.classList.add('btn-danger')
+                btnFavoritos.textContent = 'Guardar Favoritos'
+            } else {
+                mostrarToast('Receta añadida a favoritos')
+                guardarFavorito(id)
+                btnFavoritos.classList.remove('btn-danger')
+                btnFavoritos.classList.add('btn-warning')
+                btnFavoritos.textContent = 'Borrar Favorito'
+            }
+        }
+        if(cursor === btnCerrar){
+            modal.hide()
+        }
+        if (!modalTitle.contains(cursor) && !modalBody.contains(cursor) && !modalFooter.contains(cursor)) {
+            modal.hide()
+        }
+    })
+    modal.show()
+}
+
+function guardarFavorito(id) {
+    const favoritos = JSON.parse(localStorage.getItem('favoritos')) || []
+    if (!favoritos.includes(id)) {
+        favoritos.push(id)
+    }
+    localStorage.setItem('favoritos', JSON.stringify(favoritos))
+}
+
+function removerFavorito(id) {
+    let favoritos = JSON.parse(localStorage.getItem('favoritos')) || []
+    favoritos = favoritos.filter(fav => fav !== id)
+    localStorage.setItem('favoritos', JSON.stringify(favoritos))
 }
 
 function obtenerDatosApi(){
