abbbc5227db80f9c3d43999c23c35a1f934db080
DanielSaborido <dsaborio@iesrafaelalberti.es>
2023-12-08 12:11:48 +0100

    arreglo de compatibilidad del repositorio

--- DWEServidor/juego palabras/src/main/java/com/example/juegopalabras/modelo/Dificultad.java
+++
@@ -1,7 +0,0 @@
-package com.example.juegopalabras.modelo;
-
-public enum Dificultad {
-    FACIL,
-    NORMAL,
-    DIFICIL
-}
--- DWEServidor/juego palabras/src/main/java/com/example/juegopalabras/repos/EquipoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.juegopalabras.repos;
-
-import com.example.juegopalabras.modelo.Equipo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EquipoRepository extends JpaRepository<Equipo, Long> {
-
-}
