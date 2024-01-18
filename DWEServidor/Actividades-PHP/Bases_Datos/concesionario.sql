1f7e405178e6a798511f7b1e7635e61b59e512b2
DanielSaborido <dsaborio@iesrafaelalberti.es>
2024-01-18 18:19:47 +0100

    base de concesionario php

--- DWEServidor/Actividades-PHP/Bases_Datos/concesionario.sql
+++ DWEServidor/Actividades-PHP/Bases_Datos/concesionario.sql
@@ -1,45 +1,51 @@
-CREATE DATABASE IF NOT EXISTS concesionario CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-USE concesionario;
+CREATE DATABASE IF NOT EXISTS concesionario
+USE concesionario
 
 -- Tabla Coche
-CREATE TABLE Coche (
-    id INT(10) PRIMARY KEY NOT NULL,
-    Modelo VARCHAR(100),
-    Marca VARCHAR(50),
-    Precio INT(20),
-    Stock INT(20)
-);
+CREATE TABLE IF NOT EXISTS coche
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    modelo VARCHAR(100),
+    marca VARCHAR(50),
+    precio DECIMAL(12,2),
+    stock DECIMAL(5)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- Tabla Clientes
-CREATE TABLE Clientes (
-    id INT(10) PRIMARY KEY NOT NULL,
-    Nombre VARCHAR(100),
-    Ciudad VARCHAR(100)
-);
+-- Tabla cliente
+CREATE TABLE cliente
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100),
+    ciudad VARCHAR(100),
+    gastado DECIMAL(12,2)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- Tabla Encargos
-CREATE TABLE Encargos (
-    id INT(10) PRIMARY KEY NOT NULL,
-    cliente_id INT(10),
-    coche_id INT(10),
-    Cantidad INT(20),
-    Gastado FLOAT(50,2),
-    Fecha DATE,
-    FOREIGN KEY (cliente_id) REFERENCES Clientes(id),
-    FOREIGN KEY (coche_id) REFERENCES Coche(id)
-);
+-- Tabla encargo
+CREATE TABLE IF NOT EXISTS encargo
+(
+    id BIGINT AUTO_INCREMENT,
+    coche_id BIGINT,
+    cliente_id BIGINT,
+    cantidad DECIMAL(4),
+    fecha date,
+    CONSTRAINT fk_encargo_cliente FOREIGN KEY(cliente_id) REFERENCES cliente(id) ON DELETE NO ACTION,
+    CONSTRAINT fk_encargo_coche FOREIGN KEY(coche_id) REFERENCES coche(id) ON DELETE NO ACTION,
+    CONSTRAINT pk_encargo PRIMARY KEY(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+
 
 -- Insertar datos en la tabla Coche
 INSERT INTO Coche (id, Modelo, Marca, Precio, Stock)
 VALUES (1, 'Modelo1', 'Marca1', 20000, 10),
-       (2, 'Modelo2', 'Marca2', 25000, 8);
+       (2, 'Modelo2', 'Marca2', 25000, 8)
 
 -- Insertar datos en la tabla Clientes
 INSERT INTO Clientes (id, Nombre, Ciudad)
 VALUES (1, 'Cliente1', 'Ciudad1'),
-       (2, 'Cliente2', 'Ciudad2');
+       (2, 'Cliente2', 'Ciudad2')
 
 -- Insertar datos en la tabla Encargos
 INSERT INTO Encargos (id, cliente_id, coche_id, Cantidad, Gastado, Fecha)
 VALUES (1, 1, 1, 2, 40000, CURDATE()),
-       (2, 2, 2, 1, 25000, CURDATE());
+       (2, 2, 2, 1, 25000, CURDATE())
