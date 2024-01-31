d5605b01ab8f31b38fe988433a5c718438f43cbb
DanielSaborido <dsaborio@iesrafaelalberti.es>
2024-01-31 10:52:04 +0100

    modo claro y oscuro intercambiable

--- DWECliente/practicas-vue/proyecto_por_CDN/script.js
+++ DWECliente/practicas-vue/proyecto_por_CDN/script.js
@@ -73,3 +73,18 @@ const app = Vue.createApp(
 )
 
 app.mount("#vueApp")
+
+const preferedColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+const slider = document.getElementById('slider');
+
+const setTheme = (theme) => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+}
+
+slider.addEventListener('click', ()  => {
+    let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+    setTheme(switchToTheme);
+});
+
+setTheme(localStorage.getItem('theme') || preferedColorScheme);
