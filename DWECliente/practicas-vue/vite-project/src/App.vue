a0793bfdd62c137e70e8e6dc8c344ed8a3b5ebe0
DanielSaborido <dsaborio@iesrafaelalberti.es>
2024-02-01 09:38:33 +0100

    props y propiedades computadas

--- DWECliente/practicas-vue/vite-project/src/App.vue
+++ DWECliente/practicas-vue/vite-project/src/App.vue
@@ -1,30 +1,17 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Counter />
+  <Counter title="Esto es un contador" />
+  <Counter title="Esto es un contador" start="20"/>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script>
+  import Counter from "./components/Counter.vue"
+
+  export default {
+    components: {Counter}
+  }
+</script>
+
+<style>
+
 </style>
---
+++ DWECliente/practicas-vue/vite-project/src/components/Counter.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1>Counter</h1>
+    <h2>{{ title }}</h2>
+    <p>{{ counter }} <sub>2</sub> = {{ getScuareComputed }}</p>
+    <p>{{ counter }} <sub>2</sub> = {{ getScuareComputed }}</p>
+    <p>{{ counter }} <sub>2</sub> = {{ getScuareComputed }}</p>
+    <p>{{ counter }} <sub>2</sub> = {{ getScuare() }}</p>
+    <button v-on:click=(counter++)>Incrementar contador</button>
+    <button v-on:click=(counter--)>Decrementar contador</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props:{
+        title: String,
+        start: {
+            type: Number,
+            required: false,
+            default: 10
+        }
+    },
+
+    data() {
+        return{
+            counter: this.start,
+        }
+    },
+
+    methods:{
+        getScuare(){
+            console.log("Resultado calculado")
+            return this.counter*this.counter
+        }
+    },
+
+    computed: {
+        //almacena los resultados en cache para que no se tenga que llamar constantemente (propiedad computada)
+        //no puede recibir argumentos
+        //no usan parentesis para ser llamadas
+        getScuareComputed(){
+            console.log("Resultado calculado")
+            return this.counter*this.counter
+        }
+    }
+  } 
+</script>
+
+<style>
+
+</style>
